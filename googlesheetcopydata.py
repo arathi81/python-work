12577729d0857a67ab7aedbd653b05201702fda6
AmitR <amitrathi.workusa@gmail.com>
2024-04-19 00:59:48 -0400

    Copy data from one google sheet to another sheet

---
+++ googlesheetcopydata.py
@@ -0,0 +1,46 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+# Define the scope and credentials for accessing Google Sheets API
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('D:\\CODE\\python\\smart-firmament-420716-edcfe5b6cccc.json', scope)
+
+# Authenticate with Google Sheets API
+client = gspread.authorize(credentials)
+
+# Open the source Google Sheet for reading and select the first sheet
+source_sheet = client.open('sourcesheet').sheet1
+
+# Select the first sheet
+sheet = source_sheet.spreadsheet
+
+# Get all values from the sheet
+#records_data = source_sheet.get_all_records()
+sheetData = source_sheet.get_all_values()
+
+#print(records_data1)
+
+# Print the content of the spreadsheet
+for row in sheetData:
+    print(row)
+
+# Open the destination Google Sheet for writing
+destination_sheet = client.open('DestinationSheetName').sheet1
+
+# Get all values from the source sheet
+#all_values = source_sheet.get_all_values()
+
+# Iterate over each row in the source sheet
+for row in sheetData:
+    # Define the format for the destination sheet (modify as needed)
+    formatted_row = [
+        row[0],  # Example: Copying column A from source to column B in destination
+        row[1],  # Example: Copying column B from source to column D in destination
+        row[2],  # Example: Copying column C from source to column A in destination
+        # Add more columns as needed
+    ]
+    
+    # Append the formatted row to the destination sheet
+    destination_sheet.append_row(formatted_row)
+
+print("Data copied successfully to the destination sheet.")
